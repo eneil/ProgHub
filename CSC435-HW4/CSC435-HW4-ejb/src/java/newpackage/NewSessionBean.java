534460549edabba30cd5f2b7de6efa8388d22f35
Ben Gordon <Ben Gordon@BenGordon-XPS.home>
2013-04-05 12:09:20 -0400

    testing stuff....

---
+++ CSC435-HW4/CSC435-HW4-ejb/src/java/newpackage/NewSessionBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Ben Gordon
+ */
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}

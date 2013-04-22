cc68cfa2c45ca7526090748801bb23c179bbbe5c
Ben Gordon <Ben Gordon@BenGordon-XPS.home>
2013-04-21 23:19:21 -0400

    Adding Database components for User stuff, table called "userlist"

--- CSC435-HW4/CSC435-HW4-ejb/src/java/ControlEJB/AddUser.java
+++ CSC435-HW4/CSC435-HW4-ejb/src/java/ControlEJB/AddUser.java
@@ -5,7 +5,10 @@
 package ControlEJB;
 
 import Model.User;
+import Model.Userlist;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -13,13 +16,32 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class AddUser implements AddUserLocal {
+    
+    @PersistenceContext
+    EntityManager em;
 
     @Override
     public void addUser(User newUser) {
-        Model.AllUsers.allUsers.add(newUser);
+    //    Model.AllUsers.allUsers.add(newUser);  OLD EJB CODE
+        
+        Userlist tempUser = new Userlist();
+        tempUser.setFName(newUser.getFirstName());
+        tempUser.setLName(newUser.getLastName());
+        tempUser.setUsername(newUser.getUsername());
+        tempUser.setPassword(newUser.getPassword());
+        tempUser.setEMail(newUser.getEmail());
+        tempUser.setCertifications(newUser.getCertifications());
+        tempUser.setCurrentLocation(newUser.getCurrentLocation());
+        tempUser.setEducation(newUser.getEducation());
+        tempUser.setHobbies(newUser.getHobbies());
+        tempUser.setOccupation(newUser.getOccupation());
+        tempUser.setJobTitle(newUser.getJobTitle());
+        tempUser.setSkills(newUser.getSkills());
+        em.persist(tempUser);
+        
+        
     }
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+
 
 }

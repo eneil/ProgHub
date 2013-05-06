5a11f1e5751aaa149ecc6fb21cc5961186ae3768
shawnkrecker <shawnkrecker@192.168.1.7>
2013-05-05 20:09:27 -0400

    added Session Beans for Messaging Service

---
+++ CSC435-HW4/CSC435-HW4-ejb/src/java/ControlEJB/GetMessagesBean.java
@@ -0,0 +1,32 @@
+package ControlEJB;
+
+import Model.MessagesEntity;
+import java.util.List;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+/**
+ *
+ * @author shawnkrecker
+ */
+
+@Stateful
+
+
+public class GetMessagesBean implements GetMessagesBeanLocal {
+
+    @PersistenceContext
+    EntityManager em;
+    
+    public List<MessagesEntity> getMessageList(String user){
+        
+        
+        List<MessagesEntity> list = em.createQuery("Select m from MessagesEntity m WHERE m.recipient LIKE :recipientName")
+                .setParameter("recipientName", user)
+                .getResultList();
+        
+        return list;
+
+    }
+
+}
---
+++ CSC435-HW4/CSC435-HW4-ejb/src/java/ControlEJB/GetMessagesBeanLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ControlEJB;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author shawnkrecker
+ */
+@Local
+public interface GetMessagesBeanLocal {
+    
+}
